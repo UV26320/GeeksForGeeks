c91375d6dd8e90790a6ebec828725b21a802d1ee
UDAY VARA <udayvara26@gmail.com>
2023-09-28 16:33:57 +0530

    Update piece of code

---
+++ Merge two sorted linked lists/Merge two sorted linked lists.cpp
@@ -0,0 +1,140 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *sortedMerge(struct Node *a, struct Node *b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int data;
+        cin >> data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin >> data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin >> data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for (int i = 1; i < m; i++)
+        {
+            cin >> data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+// Function to merge two sorted linked list.
+
+Node *solve(Node *head1, Node *head2)
+{
+
+    // if head1 list have only one node,point its next to entire head2 list
+    if (head1->next == NULL)
+    {
+        head1->next = head2;
+        return head1;
+    }
+
+    Node *curr1 = head1;
+    Node *next1 = curr1->next;
+
+    Node *curr2 = head2;
+    Node *next2 = curr2->next;
+
+    while (next1 != NULL && curr2 != NULL)
+    {
+
+        if ((curr2->data >= curr1->data) &&
+            (curr2->data <= next1->data))
+        {
+
+            // Add node bet head1 list
+            curr1->next = curr2;
+            next2 = curr2->next;
+            curr2->next = next1;
+
+            // Updating pointer
+            curr1 = curr2;
+            curr2 = next2;
+        }
+        else
+        {
+            curr1 = next1;
+            next1 = next1->next;
+        }
+    }
+    return head1;
+}
+
+Node *sortedMerge(Node *head1, Node *head2)
+{
+    // code here
+
+    if (head1 == NULL)
+        return head2;
+
+    if (head2 == NULL)
+        return head1;
+
+    if (head1->data <= head2->data)
+       return solve(head1, head2);
+    else{
+       Node* result = solve(head2, head1);
+       return result;
+    }
+}
