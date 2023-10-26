7faf317751e03c0ddd11a2f17ce5a0ccc8e1c0a9
UDAY VARA <udayvara26@gmail.com>
2023-10-26 20:06:37 +0530

    Solution.cpp Added

---
+++ Minimum Operations/Minimum Operations.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int minOperation(int n)
+    {
+        int steps = 0;
+        while (n != 0)
+        {
+            if (n % 2 == 0)
+                n = n / 2;
+            else
+                n = n - 1;
+            steps++;
+        }
+        return steps;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperation(n) << endl;
+    }
+}
+// } Driver Code Ends
