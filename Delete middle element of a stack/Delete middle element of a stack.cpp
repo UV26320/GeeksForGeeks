b6d13f616bfa0bd8e548432a94e58be2456a9ac2
UDAY VARA <udayvara26@gmail.com>
2023-11-08 17:31:17 +0530

    Update piece of code

---
+++ Delete middle element of a stack/Delete middle element of a stack.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    void solve(stack<int> &s, int count, int size)
+    {
+
+        // Base case
+
+        if (count == size / 2)
+        {
+            s.pop();
+            return;
+        }
+
+        int num = s.top();
+        s.pop();
+
+        // Recursive call
+
+        solve(s, count + 1, size);
+
+        s.push(num);
+    }
+
+    // Function to delete middle element of a stack.
+    void deleteMid(stack<int> &s, int sizeOfStack)
+    {
+        // code here..
+
+        int count = 0;
+        solve(s, count, sizeOfStack);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int sizeOfStack;
+        cin >> sizeOfStack;
+
+        stack<int> myStack;
+
+        for (int i = 0; i < sizeOfStack; i++)
+        {
+            int x;
+            cin >> x;
+            myStack.push(x);
+        }
+
+        Solution ob;
+        ob.deleteMid(myStack, myStack.size());
+        while (!myStack.empty())
+        {
+            cout << myStack.top() << " ";
+            myStack.pop();
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
