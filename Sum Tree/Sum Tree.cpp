a886abe3121e27f896acbec65d1c71fe40f5edf1
UDAY VARA <udayvara26@gmail.com>
2023-11-25 23:26:50 +0530

    Sum Tree.cpp Added

--- Sum Tree/Sum Tree.cpp
+++ Sum Tree/Sum Tree.cpp
@@ -9,79 +9,81 @@ struct Node
     struct Node *right;
 };
 // Utility function to create a new Tree Node
-Node* newNode(int val)
+Node *newNode(int val)
 {
-    Node* temp = new Node;
+    Node *temp = new Node;
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
+
     return temp;
 }
 // Function to Build Tree
-Node* buildTree(string str)
-{   
+Node *buildTree(string str)
+{
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-        
+    Node *root = newNode(stoi(ip[0]));
+
     // Push the root to the queue
-    queue<Node*> queue;
+    queue<Node *> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size())
+    {
+
         // Get and remove the front of the queue
-        Node* currNode = queue.front();
+        Node *currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N")
+        {
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N")
+        {
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
-
 
 // } Driver Code Ends
 /*  Tree node
@@ -94,46 +96,51 @@ struct Node
 // Should return true if tree is Sum Tree, else false
 class Solution
 {
-    public:
-    
-    pair<bool,int>isSumTreeFast(Node* root){
-        
+public:
+    pair<bool, int> isSumTreeFast(Node *root)
+    {
+
         // base case
-        if(root == NULL){
-            pair<bool,int>q = make_pair(true,0);
+        if (root == NULL)
+        {
+            pair<bool, int> q = make_pair(true, 0);
             return q;
         }
-        
-        if(root->left == NULL && root->right == NULL){
-            pair<bool,int>q = make_pair(true,root->data);
+
+        if (root->left == NULL && root->right == NULL)
+        {
+            pair<bool, int> q = make_pair(true, root->data);
             return q;
         }
-        
-        pair<bool,int> leftAns = isSumTreeFast(root->left);
-        pair<bool,int> rightAns = isSumTreeFast(root->right);
-        
+
+        pair<bool, int> leftAns = isSumTreeFast(root->left);
+        pair<bool, int> rightAns = isSumTreeFast(root->right);
+
         bool left = leftAns.first;
         bool right = rightAns.first;
-        
+
         bool con = (root->data == leftAns.second + rightAns.second);
-        
-        pair<bool,int>ans;
-        
-        if(left && right && con) {
-            
+
+        pair<bool, int> ans;
+
+        if (left && right && con)
+        {
+
             ans.first = true;
-            ans.second = 2*root->data;
-        }else{
+            ans.second = 2 * root->data;
+        }
+        else
+        {
             ans.first = false;
         }
-        
+
         return ans;
     }
-    
-    bool isSumTree(Node* root)
+
+    bool isSumTree(Node *root)
     {
-         // Your code here
-         return isSumTreeFast(root).first;
+        // Your code here
+        return isSumTreeFast(root).first;
     }
 };
 
@@ -143,14 +150,14 @@ int main()
 {
 
     int t;
-	scanf("%d ",&t);
-    while(t--)
+    scanf("%d ", &t);
+    while (t--)
     {
         string s;
-		getline(cin,s);
-        Node* root = buildTree(s);
+        getline(cin, s);
+        Node *root = buildTree(s);
         Solution ob;
-        cout <<ob.isSumTree(root) << endl;
+        cout << ob.isSumTree(root) << endl;
     }
     return 1;
 }
