686e40df0cde4f369d9d2a2a2287ad1ea9fcbb32
UDAY VARA <udayvara26@gmail.com>
2023-11-29 16:28:02 +0530

    Update Piece of code

--- Tree from Postorder and Inorder/Tree from Postorder and Inorder.cpp
+++ Tree from Postorder and Inorder/Tree from Postorder and Inorder.cpp
@@ -5,20 +5,24 @@ using namespace std;
 
 /* A binary tree node has data, pointer to left child
    and a pointer to right child */
-struct Node {
+struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 
-    Node(int x) {
+    Node(int x)
+    {
         data = x;
         left = right = NULL;
     }
 };
 
 /* This funtcion is here just to test buildTreeUtil() */
-void preOrder(Node* node) {
-    if (node == NULL) return;
+void preOrder(Node *node)
+{
+    if (node == NULL)
+        return;
 
     /* then print the data of node */
     printf("%d ", node->data);
@@ -30,17 +34,21 @@ void preOrder(Node* node) {
     preOrder(node->right);
 }
 
-Node* buildTree(int in[], int post[], int n);
+Node *buildTree(int in[], int post[], int n);
 
-int main() {
+int main()
+{
     int t, n;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         cin >> n;
         int in[n], post[n];
-        for (int i = 0; i < n; i++) cin >> in[i];
-        for (int i = 0; i < n; i++) cin >> post[i];
-        Node* root = buildTree(in, post, n);
+        for (int i = 0; i < n; i++)
+            cin >> in[i];
+        for (int i = 0; i < n; i++)
+            cin >> post[i];
+        Node *root = buildTree(in, post, n);
         preOrder(root);
         cout << endl;
     }
@@ -48,7 +56,6 @@ int main() {
 }
 
 // } Driver Code Ends
-
 
 /* Tree node structure
 
@@ -64,57 +71,58 @@ struct Node
     }
 };*/
 
-//Function to return a tree created from postorder and inoreder traversals.
- void creatMapping(int in[],map<int,int> &nodeToIndex , int n){
-        
-        for(int i = 0 ; i < n ; ++i){
-            nodeToIndex[in[i]] = i ;
-        }
-    } 
-   
-   
+// Function to return a tree created from postorder and inoreder traversals.
+void creatMapping(int in[], map<int, int> &nodeToIndex, int n)
+{
+
+    for (int i = 0; i < n; ++i)
+    {
+        nodeToIndex[in[i]] = i;
+    }
+}
+
 Node *solve(int in[], int post[], int &index, int inOrderedStart,
-    int inOrderedEnd, int n,map<int,int> &nodeToIndex) {
-       
-       // base case
-        if (index <0 || inOrderedStart > inOrderedEnd)
-            return NULL;
-        
-        // create a root node for element 
-        int element = post[index--];
-        Node *root = new Node(element);
-        
-        
-        // int position = findPosition(in, element, n);
+            int inOrderedEnd, int n, map<int, int> &nodeToIndex)
+{
 
-        // find element's index in inorder
-        int position = nodeToIndex[element];
-        
+    // base case
+    if (index < 0 || inOrderedStart > inOrderedEnd)
+        return NULL;
 
-        if (position == -1) {
-            delete root;
-            return NULL;
-        }
+    // create a root node for element
+    int element = post[index--];
+    Node *root = new Node(element);
 
-        root->right = solve(in, post, index, position + 1, inOrderedEnd, n,nodeToIndex);
-        root->left = solve(in, post, index, inOrderedStart, position - 1, n,nodeToIndex);
+    // int position = findPosition(in, element, n);
 
-        return root;
+    // find element's index in inorder
+    int position = nodeToIndex[element];
+
+    if (position == -1)
+    {
+        delete root;
+        return NULL;
     }
 
+    root->right = solve(in, post, index, position + 1, inOrderedEnd, n, nodeToIndex);
+    root->left = solve(in, post, index, inOrderedStart, position - 1, n, nodeToIndex);
 
-Node *buildTree(int in[], int post[], int n) {
+    return root;
+}
+
+Node *buildTree(int in[], int post[], int n)
+{
     // Your code here
-    
-        if (n == 0) return NULL;
-        int postOrderIndex = n - 1;
-        
-        map<int,int>nodeToIndex;
-        
-        // creating nodes to index mapping 
-        creatMapping(in,nodeToIndex,n);
 
-        Node *ans = solve(in, post, postOrderIndex, 0, n - 1, n,nodeToIndex);
-        return ans;
-    
+    if (n == 0)
+        return NULL;
+    int postOrderIndex = n - 1;
+
+    map<int, int> nodeToIndex;
+
+    // creating nodes to index mapping
+    creatMapping(in, nodeToIndex, n);
+
+    Node *ans = solve(in, post, postOrderIndex, 0, n - 1, n, nodeToIndex);
+    return ans;
 }
