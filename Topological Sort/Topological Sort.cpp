d0aca740f5b6af4b048afe87d2845cedbdc5ca97
UDAY VARA <udayvara26@gmail.com>
2024-01-10 15:11:31 +0530

    khan's algo approach include

--- Topological Sort/Topological Sort.cpp
+++ Topological Sort/Topological Sort.cpp
@@ -14,6 +14,35 @@ void TopoSort(int node, vector<bool> &visited, stack<int> &s, unordered_map<int,
 
 vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)
 {
+
+    // Approach 1 = using DFS
+    // unordered_map<int, list<int>> adj;
+    // for (int i = 0; i < e; ++i) {
+    //     int u = edges[i][0];
+    //     int v = edges[i][1];
+    //     adj[u].push_back(v);
+    // }
+
+    // vector<bool> visited(v);
+    // stack<int> s;
+
+    // for (int i = 0; i < v; ++i) {
+    //     if (!visited[i])
+    //         TopoSort(i, visited, s, adj);
+    // }
+
+    // vector<int> ans;
+
+    // while (!s.empty()) {
+    //     ans.push_back(s.top());
+    //     s.pop();
+    // }
+    // return ans;
+
+    // ================================================================================
+
+    // Approach 2 = Using BFS (known as khan's Algo)
+
     unordered_map<int, list<int>> adj;
     for (int i = 0; i < e; ++i)
     {
@@ -22,21 +51,47 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)
         adj[u].push_back(v);
     }
 
-    vector<bool> visited(v);
-    stack<int> s;
+    // find all indegrees
+    vector<int> indegrees(v);
 
+    for (auto i : adj)
+    {
+        for (auto j : i.second)
+        {
+            indegrees[j]++;
+        }
+    }
+
+    // of which node indegree is zero(0) we can push them into queue
+    queue<int> q;
     for (int i = 0; i < v; ++i)
     {
-        if (!visited[i])
-            TopoSort(i, visited, s, adj);
+        if (indegrees[i] == 0)
+            q.push(i);
     }
 
+    // Do DFS for all other nodes
     vector<int> ans;
 
-    while (!s.empty())
+    while (!q.empty())
     {
-        ans.push_back(s.top());
-        s.pop();
+
+        int front = q.front();
+        q.pop();
+
+        // store ans
+        ans.push_back(front);
+
+        // neighbour indegreea update
+
+        for (auto neighbour : adj[front])
+        {
+
+            indegrees[neighbour]--;
+            if (indegrees[neighbour] == 0)
+                q.push(neighbour);
+        }
     }
+
     return ans;
 }
