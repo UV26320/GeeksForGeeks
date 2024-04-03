24d10f1307bb6bbea9d4d09ec9a8fa40a4a5a975
UDAY VARA <udayvara26@gmail.com>
2024-04-03 14:39:15 +0530

    updated piece of code

--- Kth common ancestor in BST/Kth common ancestor in BST.cpp
+++ Kth common ancestor in BST/Kth common ancestor in BST.cpp
@@ -1,168 +1,175 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // A Tree node
 struct Node
 {
-	int data;
-	struct Node *left, *right;
+    int data;
+    struct Node *left, *right;
 };
 
 // Utility function to create a new node
-Node* newNode(int data)
+Node *newNode(int data)
 {
-	Node* temp = new Node;
-	temp->data = data;
-	temp->left = temp->right = NULL;
-	return (temp);
+    Node *temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return (temp);
 }
 
-Node* buildTree(string str)
+Node *buildTree(string str)
 {
-	// Corner Case
-	if (str.length() == 0 || str[0] == 'N')
-		return NULL;
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
 
-	// Creating vector of strings from input
-	// string after spliting by space
-	vector<string> ip;
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
 
-	istringstream iss(str);
-	for (string str; iss >> str; )
-		ip.push_back(str);
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
 
-	// Create the root of the tree
-	Node* root = newNode(stoi(ip[0]));
+    // Create the root of the tree
+    Node *root = newNode(stoi(ip[0]));
 
-	// Push the root to the queue
-	queue<Node*> queue;
-	queue.push(root);
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
 
-	// Starting from the second element
-	int i = 1;
-	while (!queue.empty() && i < ip.size()) {
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
 
-		// Get and remove the front of the queue
-		Node* currNode = queue.front();
-		queue.pop();
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
 
-		// Get the current node's value from the string
-		string currVal = ip[i];
+        // Get the current node's value from the string
+        string currVal = ip[i];
 
-		// If the left child is not null
-		if (currVal != "N") {
+        // If the left child is not null
+        if (currVal != "N")
+        {
 
-			// Create the left child for the current node
-			currNode->left = newNode(stoi(currVal));
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
 
-			// Push it to the queue
-			queue.push(currNode->left);
-		}
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
 
-		// For the right child
-		i++;
-		if (i >= ip.size())
-			break;
-		currVal = ip[i];
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
 
-		// If the right child is not null
-		if (currVal != "N") {
+        // If the right child is not null
+        if (currVal != "N")
+        {
 
-			// Create the right child for the current node
-			currNode->right = newNode(stoi(currVal));
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
 
-			// Push it to the queue
-			queue.push(currNode->right);
-		}
-		i++;
-	}
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
 
-	return root;
+    return root;
 }
-
 
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
 /*// A Tree node
 struct Node
 {
-	int data;
-	struct Node *left, *right;
+    int data;
+    struct Node *left, *right;
 };*/
-
 
 class Solution
 {
-    private:
-    
-    Node* LCA(Node* root,int x , int y){
-        if(!root) return NULL;
-        if(root->data == x || root->data == y) return root;
-        else if(root->data < x && root->data < y) return LCA(root->right,x,y);
-        else if(root ->data > x && root->data > y) return LCA(root->left,x,y);
-        else{
+private:
+    Node *LCA(Node *root, int x, int y)
+    {
+        if (!root)
+            return NULL;
+        if (root->data == x || root->data == y)
+            return root;
+        else if (root->data < x && root->data < y)
+            return LCA(root->right, x, y);
+        else if (root->data > x && root->data > y)
+            return LCA(root->left, x, y);
+        else
+        {
             return root;
         }
     }
-    
-    void f(Node*root , Node* lca,vector<int>&path){
+
+    void f(Node *root, Node *lca, vector<int> &path)
+    {
         path.push_back(root->data);
-        if(root->data == lca->data) return;
-        else if(root->data > lca -> data) f(root->left,lca,path);
-        else{
-            f(root->right,lca,path);
+        if (root->data == lca->data)
+            return;
+        else if (root->data > lca->data)
+            f(root->left, lca, path);
+        else
+        {
+            f(root->right, lca, path);
         }
         return;
     }
-    
-    public:
-    
+
+public:
     /*You are required to complete below function */
-    int kthCommonAncestor(Node *root, int k,int x, int y)
+    int kthCommonAncestor(Node *root, int k, int x, int y)
     {
         // your code goes here
-        Node* lca = LCA(root,x,y);
-        vector<int>path;
-        f(root,lca,path);
+        Node *lca = LCA(root, x, y);
+        vector<int> path;
+        f(root, lca, path);
         int n = path.size();
-        if(path.size()<k) return -1;
-        return path[n-k];
+        if (path.size() < k)
+            return -1;
+        return path[n - k];
     }
 };
 
-
-
 //{ Driver Code Starts.
 
-int main ()
+int main()
 {
 
-	int t;
-	cin >> t;
-	cin.ignore();
+    int t;
+    cin >> t;
+    cin.ignore();
 
-	while (t--)
-	{
-	
-		string inp;
-		getline(cin, inp);
-		
-		int k,x,y;
-		cin>>k>>x>>y;
-		cin.ignore();
+    while (t--)
+    {
 
-		struct Node* root = buildTree(inp);
+        string inp;
+        getline(cin, inp);
+
+        int k, x, y;
+        cin >> k >> x >> y;
+        cin.ignore();
+
+        struct Node *root = buildTree(inp);
 
         Solution ob;
-		int ans = ob.kthCommonAncestor(root,k,x,y);
-		cout<<ans<<endl;
+        int ans = ob.kthCommonAncestor(root, k, x, y);
+        cout << ans << endl;
+    }
 
-	}
-
-	return 0;
+    return 0;
 }
 // } Driver Code Ends
