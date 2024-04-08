d2365816d11af02dc401a053122c86bbddb58fbe
UDAY VARA <udayvara26@gmail.com>
2024-04-08 13:57:53 +0530

    solution.cpp updated

--- 1700. Number of Students Unable to Eat Lunch/Solution.cpp
+++ 1700. Number of Students Unable to Eat Lunch/Solution.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) {
-            
-            int circleSandwich = 0;
-            int squareSandwich = 0;
+    int countStudents(vector<int> &students, vector<int> &sandwiches)
+    {
 
-            for(int choice : students){
-                if(choice == 0) circleSandwich++;
-                else squareSandwich++;
-            }
+        int circleSandwich = 0;
+        int squareSandwich = 0;
 
-            for(int sandwich : sandwiches ){
-                
-                // base case
-                if(sandwich == 0 && circleSandwich == 0) return squareSandwich;
-                if(sandwich == 1 && squareSandwich == 0) return circleSandwich;
-                
-                if(sandwich == 0) circleSandwich--;
-                else squareSandwich--;
+        for (int choice : students)
+        {
+            if (choice == 0)
+                circleSandwich++;
+            else
+                squareSandwich++;
+        }
 
-            }
+        for (int sandwich : sandwiches)
+        {
 
-            return 0;
+            // base case
+            if (sandwich == 0 && circleSandwich == 0)
+                return squareSandwich;
+            if (sandwich == 1 && squareSandwich == 0)
+                return circleSandwich;
+
+            if (sandwich == 0)
+                circleSandwich--;
+            else
+                squareSandwich--;
+        }
+
+        return 0;
     }
 };
