ea874a1d9c497db78c2d159fb155dc3b43dc5a48
UDAY VARA <udayvara26@gmail.com>
2024-04-12 14:04:25 +0530

    updated

---
+++ 402. Remove K Digits/SOLution.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string removeKdigits(string num, int k)
+    {
+        stack<char> stack;
+
+        for (char digit : num)
+        {
+            while (!stack.empty() && k > 0 && stack.top() > digit)
+            {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+
+        // Remove remaining k digits from the end of the stack
+        while (k > 0 && !stack.empty())
+        {
+            stack.pop();
+            k--;
+        }
+
+        // Construct the resulting string from the stack
+        string result;
+        while (!stack.empty())
+        {
+            result += stack.top();
+            stack.pop();
+        }
+        reverse(result.begin(), result.end()); // Reverse to get the correct order
+
+        // Remove leading zeros
+        size_t pos = result.find_first_not_of('0');
+        result = (pos == string::npos) ? "0" : result.substr(pos);
+
+        return result;
+    }
+};
