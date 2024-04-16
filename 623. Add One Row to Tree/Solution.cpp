7e93b3e3e57ec2a1445cdb25a1f6cea0211ed2a2
UDAY VARA <udayvara26@gmail.com>
2024-04-16 14:32:58 +0530

    solution.cpp updated

--- 623. Add One Row to Tree/Solution.cpp
+++ 623. Add One Row to Tree/Solution.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
 class Solution
 {
 public:
