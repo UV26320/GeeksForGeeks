17ca13f9f6f9ef5ec10457236b686e8ab21e1bc0
UDAY VARA <udayvara26@gmail.com>
2024-04-21 15:39:31 +0530

    solution.cpp updated

--- 1971. Find if Path Exists in Graph/Solution.cpp
+++ 1971. Find if Path Exists in Graph/Solution.cpp
@@ -1,24 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
 private:
-    
+    bool dfs(int node, int destination, unordered_map<int, vector<int>> &graph, unordered_set<int> &visited)
+    {
+
+        // base case
+        if (node == destination)
+            return true;
+
+        visited.insert(node);
+
+        for (int neighbour : graph[node])
+        {
+            if (visited.find(neighbour) == visited.end())
+            {
+                if (dfs(neighbour, destination, graph, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+
 public:
-   bool vailPath(int n ,vector<vector<int>>& edges,int source, int destination){
-     unordered_map<int,vector<int>>graph;
+    bool vailPath(int n, vector<vector<int>> &edges, int source, int destination)
+    {
+        unordered_map<int, vector<int>> graph;
 
-     for (const auto& edge : edges) 
-     {
-        int u = edge[0];
-        int v = edge[1];
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-     }
+        for (const auto &edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
 
-     unordered_set<int> visited;
-     return dfs(source,destination, graph, visited);
-     
-   }
+        unordered_set<int> visited;
+        return dfs(source, destination, graph, visited);
+    }
 };
