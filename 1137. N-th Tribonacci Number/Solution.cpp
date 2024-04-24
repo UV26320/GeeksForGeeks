ba3933ed4571330e168f1dcbcbecb3a8446ddde8
UDAY VARA <udayvara26@gmail.com>
2024-04-24 19:40:30 +0530

    solution.cpp updated

--- 1137. N-th Tribonacci Number/Solution.cpp
+++ 1137. N-th Tribonacci Number/Solution.cpp
@@ -1,21 +1,29 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
 private:
-    int solve(int n, vector<int>& memo) {
+    int solve(int n, vector<int> &memo)
+    {
         // base cases
-        if (n == 0) return 0;
-        if (n == 1 || n == 2) return 1;
-        
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+
         // check if already calculated
         if (memo[n] != -1)
             return memo[n];
-        
+
         // calculate Tribonacci recursively
-        memo[n] = solve(n-1, memo) + solve(n-2, memo) + solve(n-3, memo);
+        memo[n] = solve(n - 1, memo) + solve(n - 2, memo) + solve(n - 3, memo);
         return memo[n];
     }
 
 public:
-    int tribonacci(int n) {
+    int tribonacci(int n)
+    {
         // Initialize memoization array
         vector<int> memo(n + 1, -1);
         return solve(n, memo);
