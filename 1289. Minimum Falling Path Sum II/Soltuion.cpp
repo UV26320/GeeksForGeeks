e6f22cbd90e42edf1dff31b8db7d612fe6b38ba1
UDAY VARA <udayvara26@gmail.com>
2024-04-26 21:14:40 +0530

    solution updated

--- 1289. Minimum Falling Path Sum II/Soltuion.cpp
+++ 1289. Minimum Falling Path Sum II/Soltuion.cpp
@@ -1,22 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int minFallingPathSum(vector<vector<int>>& grid) {
+    int minFallingPathSum(vector<vector<int>> &grid)
+    {
         int n = grid.size(), res = INT_MAX;
         vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        for(int j = 0; j < n; ++j) {
+        for (int j = 0; j < n; ++j)
+        {
             dp[0][j] = grid[0][j];
         }
 
-        for(int i = 1; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
                 int temp = INT_MAX;
 
-                for(int k = 0; k < n; ++k) {
-                    if(k != j) {
+                for (int k = 0; k < n; ++k)
+                {
+                    if (k != j)
+                    {
                         temp = min(temp, grid[i][j] + dp[i - 1][k]);
                     }
 
@@ -25,7 +32,8 @@ public:
             }
         }
 
-        for(int j = 0; j < n; ++j) {
+        for (int j = 0; j < n; ++j)
+        {
             res = min(res, dp[n - 1][j]);
         }
 
