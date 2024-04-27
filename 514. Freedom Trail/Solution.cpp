dc9b229cd2a8b7c8234721fa2355afefb9a31104
UDAY VARA <udayvara26@gmail.com>
2024-04-27 16:42:03 +0530

    optimal solution added

--- 514. Freedom Trail/Solution.cpp
+++ 514. Freedom Trail/Solution.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution
 {
 private:
-    int solve(string &ring, string &key, int ptr, int index)
+    int solve(string &ring, string &key, int ptr, int index , vector<vector<int>> &dp)
     {
 
         int n = ring.size();
@@ -13,6 +13,9 @@ private:
         // base case
         if (index >= m)
             return 0;
+        
+        if(dp[index][ptr] != -1) return dp[index][ptr];
+        
 
         // recursive call (choices...)
         int steps = INT_MAX;
@@ -24,19 +27,20 @@ private:
                 steps = min(steps, min(abs(i - ptr), n - abs(i - ptr)));
             }
         }
-        return steps;
+        return dp[index][ptr] = steps;
     }
 
 public:
     int findRotateSteps(string ring, string key)
     {
-
+        
         int ptr = 0;
         int index = 0;
 
         int n = ring.size();
         int m = key.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
 
-        solve(ring, key, ptr, index);
+        solve(ring, key, ptr, index,dp);
     }
 };
