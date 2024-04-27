78c4727490e792a4e34a813ed8d91aefb9cd295d
UDAY VARA <udayvara26@gmail.com>
2024-04-27 16:04:50 +0530

    solution added

--- 514. Freedom Trail/Solution.cpp
+++ 514. Freedom Trail/Solution.cpp
@@ -1,3 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+private:
+    int solve(string &ring, string &key, int ptr, int index)
+    {
+
+        int n = ring.size();
+        int m = key.size();
+
+        // base case
+        if (index >= m)
+            return 0;
+
+        // recursive call (choices...)
+        int steps = INT_MAX;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (ring[i] == key[index])
+            {
+                steps = min(steps, min(abs(i - ptr), n - abs(i - ptr)));
+            }
+        }
+        return steps;
+    }
+
+public:
+    int findRotateSteps(string ring, string key)
+    {
+
+        int ptr = 0;
+        int index = 0;
+
+        int n = ring.size();
+        int m = key.size();
+
+        solve(ring, key, ptr, index);
+    }
+};
