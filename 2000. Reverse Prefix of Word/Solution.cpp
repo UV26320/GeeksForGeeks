ca28ca1e3fa899163056672879750ae2528d0600
UDAY VARA <udayvara26@gmail.com>
2024-05-01 13:34:25 +0530

    solution.cpp added

---
+++ 2000. Reverse Prefix of Word/Solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+
+        int n = word.find(ch);
+        if (ch == -1)
+            return word;
+        int r0 = (n - 1) / 2;
+        for (int i = 0; i <= r0; i++)
+            swap(word[i], word[n - i]);
+        return word;
+    }
+};
