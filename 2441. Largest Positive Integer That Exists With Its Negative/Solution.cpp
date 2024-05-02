5f39b38c69ff5b8ab9b1ba9b45ecb6dd9b71e23d
UDAY VARA <udayvara26@gmail.com>
2024-05-02 11:43:47 +0530

    solution updated

--- 2441. Largest Positive Integer That Exists With Its Negative/Solution.cpp
+++ 2441. Largest Positive Integer That Exists With Its Negative/Solution.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findMaxK(vector<int>& nums) {
-        
-        sort(nums.begin(),nums.end());
+    int findMaxK(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = n -1 ; i>=0 ;--i){
-            if(nums[i] > 0 and binary_search(nums.begin(), nums.end(), -nums[i])) return nums[i];
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (nums[i] > 0 and binary_search(nums.begin(), nums.end(), -nums[i]))
+                return nums[i];
         }
 
         return -1;
