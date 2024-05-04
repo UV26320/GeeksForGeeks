22ae3e469889b38b155846bf0ad09fb9a214c88b
UDAY VARA <udayvara26@gmail.com>
2024-05-04 19:26:10 +0530

    solution.cpp Added

---
+++ 881. Boats to Save People/Solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int n = people.size();
+        int left = 0, right = n - 1;
+        int NoBots = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                NoBots++;
+                left++;
+                right--;
+            } else {
+                NoBots++;
+                right--;
+            }
+        }
+
+        return NoBots;
+    }
+};
