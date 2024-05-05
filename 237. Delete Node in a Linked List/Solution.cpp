385eaf9cf78423df06fc85d4f1920e66e1266aba
UDAY VARA <udayvara26@gmail.com>
2024-05-05 12:44:35 +0530

    Solution added

---
+++ 237. Delete Node in a Linked List/Solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+     int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+  
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        // base case
+        if(node == nullptr) return;
+
+        node->val = node -> next -> val;
+        node -> next = node -> next -> next;
+
+    }
+};
