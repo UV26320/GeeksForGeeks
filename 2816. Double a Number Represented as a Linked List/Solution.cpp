32043086c4f11067d4658f6b47f79e6d2111e323
UDAY VARA <udayvara26@gmail.com>
2024-05-07 18:42:46 +0530

    2816. Double a Number Represented as a Linked List added

--- 2816. Double a Number Represented as a Linked List/Solution.cpp
+++ 2816. Double a Number Represented as a Linked List/Solution.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 /**
@@ -12,16 +12,40 @@ using namespace std;
  * };
  */
 
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {} };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-class Solution {
-public:
-    ListNode* doubleIt(ListNode* head) {
+class Solution
+{
+private:
+    int solve(ListNode *head)
+    {
+
+        // base case
+        if (head == nullptr)
+            return 0;
+
+        int value = head->val * 2 + solve(head->next);
+        head->val = value % 10;
         
+        // carry
+        int ans = value / 10;
+        return ans;
+    }
+
+public:
+    ListNode *doubleIt(ListNode *head)
+    {
+
+        int carry = solve(head);
+        if (carry)
+            head = new ListNode(carry, head);
+        return head;
     }
 };
