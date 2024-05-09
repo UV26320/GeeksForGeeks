640b81b911f64d5723fe4ff6eb2d6ac37f7e4862
UDAY VARA <udayvara26@gmail.com>
2024-05-09 10:40:34 +0530

    3075. Maximize Happiness of Selected Children

---
+++ 3075. Maximize Happiness of Selected Children/Solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long maximumHappinessSum(vector<int> &happiness, int k)
+    {
+
+        sort(begin(happiness), end(happiness), greater<int>());
+        int i = 0;
+        long long res = 0;
+
+        while (k--)
+        {
+            happiness[i] = max(happiness[i] - i, 0);
+            res += happiness[i++];
+        }
+
+        return res;
+    }
+};
